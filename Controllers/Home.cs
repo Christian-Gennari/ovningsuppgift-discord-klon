87304ded11ad615b6b3732903a428628ee1060e9
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2026-03-05 13:29:00 +0100

    Refactor project structure: move HTML, JS, and CSS to wwwroot; remove old public files; update project configuration for new paths

--- Controllers/Home.cs
+++ Controllers/Home.cs
@@ -1,26 +1,26 @@
-using System.Net;
-using System.Net.Mime;
-using System.Runtime.Versioning;
-using System.Security.Cryptography;
-using System.Security.Cryptography.X509Certificates;
-using Microsoft.AspNetCore.Mvc;
+// using System.Net;
+// using System.Net.Mime;
+// using System.Runtime.Versioning;
+// using System.Security.Cryptography;
+// using System.Security.Cryptography.X509Certificates;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace _Kurs_webb_csharp.Controllers;
+// namespace _Kurs_webb_csharp.Controllers;
 
-[ApiController]
-[Route("/")]
-[Route("home")]
-public class Home : Controller
-{
-    [HttpGet]
-    public IActionResult Get()
-    {
-        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "public", "index.html");
+// [ApiController]
+// [Route("/")]
+// [Route("home")]
+// public class Home : Controller
+// {
+//     [HttpGet]
+//     public IActionResult Get()
+//     {
+//         var filePath = Path.Combine(Directory.GetCurrentDirectory(), "public", "index.html");
 
-        if (!System.IO.File.Exists(filePath))
-        {
-            return NotFound("The file public/index.html was not found.");
-        }
-        return PhysicalFile(filePath, "text/html");
-    }
-}
+//         if (!System.IO.File.Exists(filePath))
+//         {
+//             return NotFound("The file public/index.html was not found.");
+//         }
+//         return PhysicalFile(filePath, "text/html");
+//     }
+// }
