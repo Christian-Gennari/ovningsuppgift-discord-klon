abfeecd5bd00576eb6f4a2e81a8185438611a396
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2026-03-05 14:42:11 +0100

    Refactor message handling in index.js: implement sendMessage and getMessages functions for improved message management

--- Program.cs
+++ Program.cs
@@ -35,4 +35,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run("http://0.0.0.0:5000");
--- wwwroot/index.js
+++ wwwroot/index.js
@@ -1,27 +1,59 @@
 "use strict";
 
-const messageInput = document.getElementById('message-input');
-const nameInput = document.getElementById('name-input');
+const messageInput = document.getElementById("message-input");
+const nameInput = document.getElementById("name-input");
 
-window.addEventListener('load', async () => {
-    const response = await fetch('/api/messages');
-    const data = await response.json();
-    const list = document.getElementById('message-container');
+window.addEventListener("load", async () => {
+  await getMessages();
+});
 
+window.addEventListener("keypress", async (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    await sendMessage();
+  }
+});
 
+async function sendMessage() {
+  const message = messageInput.value;
+  const name = nameInput.value;
 
-    data.forEach(item => {
-        const userName = document.createElement('p');
-        userName.textContent = item.user;
-        userName.classList.add('user-name');
+  if (message && name) {
+    const response = await fetch("/api/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: name, messageSent: message }),
+    });
 
-        const message = document.createElement('p');
-        message.textContent = item.messageSent;
-        message.classList.add('message');
+    if (response.ok) {
+      getMessages();
+    }
+  }
+}
 
-        list.appendChild(userName);
-        list.appendChild(message);
+async function getMessages() {
+  const response = await fetch("/api/messages");
+  const data = await response.json();
+  const list = document.getElementById("message-container");
 
-        console.log(list);
-    })  
-})
+  list.innerHTML = "";
+
+  data.forEach((item) => {
+    const listItem = document.createElement("div");
+    const userName = document.createElement("p");
+    userName.textContent = item.user;
+    userName.classList.add("user-name");
+
+    const message = document.createElement("p");
+    message.textContent = item.messageSent;
+    message.classList.add("message");
+
+    listItem.appendChild(userName);
+    listItem.appendChild(message);
+    list.appendChild(listItem);
+
+    console.log(list);
+  });
+}
